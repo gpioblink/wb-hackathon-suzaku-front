90b68c9bb6f9d3d62b6202d69564c5bf1bdbc817
Kayi <gpioblink@gmail.com>
2022-11-28 14:35:30 +0900

    use development mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 import colors from 'vuetify/es5/util/colors'
 
-const environment = process.env.NODE_ENV || "development"
+const environment = process.env.NODE_ENV || "local"
 const env = require(`./env/${environment}.ts`)
 
 export default {
